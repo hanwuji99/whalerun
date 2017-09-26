11491f0218137746623fa9a761af6b2dd515af8a
hanwuji99 <hanwuji99@outlook.com>
2017-09-26 16:01:35 +0800

    提交

--- user/run.py
+++ user/run.py
@@ -2,12 +2,66 @@
 
 from app import create_app
 from app.tasks import celery
-from app.blueprints.api.views import App
+from flask import Flask, redirect, url_for, session, request, jsonify
+from flask_oauthlib.client import OAuth
 
 app = create_app()
+oauth = OAuth(app)
+
+github = oauth.remote_app(
+    'github',
+    consumer_key='1ec3398f773522ea93a2',
+    consumer_secret='356740a0ba53c57de3ef794850bedcf61bd84ef3',
+    request_token_params={'scope': 'user:email'},
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize'
+)
+#
+
+@app.route('/index')
+def index():
+    if 'github_token' in session:
+        me = github.get('user')
+        return jsonify(me.data)
+    return redirect(url_for('github_login'))
+
+
+@app.route('/users/github_login')
+def github_login():
+    return github.authorize(callback=url_for('github_authorized', _external=True))
+
+
+@app.route('/users/github_login/authorized')
+def github_authorized():
+    resp = github.authorized_response()
+    if resp is None or resp.get('access_token') is None:
+        return 'Access denied: reason=%s error=%s resp=%s' % (
+            request.args['error'],
+            request.args['error_description'],
+            resp
+        )
+    session['github_token'] = (resp['access_token'], '')
+    me = github.get('user')
+    return jsonify(me.data)
+
+
+@app.route('/users/logout')
+def logout():
+    session.pop('github_token', None)
+    return redirect(url_for('index'))
+
+
+@github.tokengetter
+def get_github_oauth_token():
+    return session.get('github_token')
+
+
 if __name__ == '__main__':
-    global App
-    App.set_app(app)
+    # global App
+    # App.set_app(app)
     app.run(debug=True)
 
 
