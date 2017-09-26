5d6449c02b4afc25f9d0bc3b66f54b8d50db59a0
hanwuji99 <hanwuji99@outlook.com>
2017-09-26 11:28:54 +0800

    提交

--- user/app/blueprints/api/views.py
+++ user/app/blueprints/api/views.py
@@ -17,7 +17,7 @@ from flask import Flask, redirect, url_for, session, request, jsonify
 from flask_oauthlib.client import OAuth
 # from . import bp_api
 class App(object):
-    
+
     app = None
     @classmethod
     def set_app(cls,app):
